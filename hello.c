81445fee9a820892cc7bf182ba3380711c41c70f
Lesly <lfonsecaramos2@huskers.unl.edu>
2025-08-28 13:08:11 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Lesly Fonseca Ramos
+ * Date: 2025/08/28
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Lesly\n");
+
+  return 0;
+}
