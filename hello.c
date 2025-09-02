c606d67c3562bd2d4e0155ecb9a4b614763e5b30
Raegan Scheet <118682668+cscheet2@users.noreply.github.com>
2025-09-02 13:06:44 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,6 +5,7 @@
  * A simple hello world program in C
  *
  */
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -12,6 +13,9 @@ int main(int argc, char **argv) {
 
   printf("Hello, I'm Lesly.\n");
   printf("Major is Electrical Engineering!\n");
+
+  printf("Hello, I'm Raegan!\n");  // NOTE: a am course leader working with a student who did not get a partner on the hack
+  printf("My major is in Software Engineering!\n");
 
   return 0;
 }
